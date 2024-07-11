781b0f6891ce0f26a80c7e51de1fa264f20d935a
viti04ek <99684929+viti04ek@users.noreply.github.com>
2024-07-11 17:35:13 +0300

    Collision detection

--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D _boxCollider;
-
     private Vector3 _moveDelta;
+    private RaycastHit2D _hit;
 
     private void Start()
     {
@@ -15,6 +15,12 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate()
+    {
+        RotatePlayer();
+        MovePlayer();
+    }
+
+    private void RotatePlayer()
     {
         var x = Input.GetAxisRaw("Horizontal");
         var y = Input.GetAxisRaw("Vertical");
@@ -24,7 +30,18 @@ public class Player : MonoBehaviour
             transform.localScale = Vector3.one;
         else if (_moveDelta.x < 0)
             transform.localScale = new(-1, 1, 1);
+    }
+
+    private void MovePlayer()
+    {
+        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new(0, _moveDelta.y),
+            Mathf.Abs(_moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (_hit.collider == null)
+            transform.Translate(0, _moveDelta.y * Time.deltaTime, 0);
         
-        transform.Translate(_moveDelta * Time.deltaTime);
+        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new(_moveDelta.x, 0),
+            Mathf.Abs(_moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (_hit.collider == null)
+            transform.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
     }
 }
