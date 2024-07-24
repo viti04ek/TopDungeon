b3bbadda2fbf627d46a996d58dfd7ebd6ec519c0
viti04ek <99684929+viti04ek@users.noreply.github.com>
2024-07-24 15:25:49 +0300

    Weapon script

---
+++ TopDungeon/Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Collidable
+{
+    public int DamagePoint { get; private set; } = 1;
+    public float PushForce { get; private set; } = 2f;
+    public int WeaponLevel { get; set; } = 0;
+    private SpriteRenderer _spriteRenderer;
+    private float _cooldown = 0.5f;
+    private float _lastSwing;
+
+    protected override void Start()
+    {
+        base.Start();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            TryToSwing();
+    }
+
+    private void TryToSwing()
+    {
+        if (Time.time - _lastSwing > _cooldown)
+        {
+            _lastSwing = Time.time;
+            Swing();
+        }
+    }
+
+    private void Swing()
+    {
+        Debug.Log("Swing");
+    }
+}
