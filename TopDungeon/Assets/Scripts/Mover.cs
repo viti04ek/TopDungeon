80cdc7ad5cb72b9fbe241fbbeb101bf09fb831d9
viti04ek <99684929+viti04ek@users.noreply.github.com>
2024-07-24 18:31:22 +0300

    Create Mover

---
+++ TopDungeon/Assets/Scripts/Mover.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : Fighter
+{
+    protected BoxCollider2D _boxCollider;
+    protected Vector3 _moveDelta;
+    protected RaycastHit2D _hit;
+    protected float _ySpeed = 0.75f;
+    protected float _xSpeed = 1f;
+
+    protected virtual void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        _moveDelta = new(input.x * _xSpeed, input.y * _ySpeed, 0);
+
+        if (_moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (_moveDelta.x < 0)
+            transform.localScale = new(-1, 1, 1);
+        
+        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new(0, _moveDelta.y),
+            Mathf.Abs(_moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (_hit.collider == null)
+            transform.Translate(0, _moveDelta.y * Time.deltaTime, 0);
+        
+        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new(_moveDelta.x, 0),
+            Mathf.Abs(_moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (_hit.collider == null)
+            transform.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
+    }
+}
--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -3,45 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Fighter
+public class Player : Mover
 {
-    private BoxCollider2D _boxCollider;
-    private Vector3 _moveDelta;
-    private RaycastHit2D _hit;
-
-    private void Start()
-    {
-        _boxCollider = GetComponent<BoxCollider2D>();
-    }
-
     private void FixedUpdate()
-    {
-        RotatePlayer();
-        MovePlayer();
-    }
-
-    private void RotatePlayer()
     {
         var x = Input.GetAxisRaw("Horizontal");
         var y = Input.GetAxisRaw("Vertical");
-        _moveDelta = new(x, y, 0);
-
-        if (_moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (_moveDelta.x < 0)
-            transform.localScale = new(-1, 1, 1);
-    }
-
-    private void MovePlayer()
-    {
-        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new(0, _moveDelta.y),
-            Mathf.Abs(_moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (_hit.collider == null)
-            transform.Translate(0, _moveDelta.y * Time.deltaTime, 0);
-        
-        _hit = Physics2D.BoxCast(transform.position, _boxCollider.size, 0, new(_moveDelta.x, 0),
-            Mathf.Abs(_moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (_hit.collider == null)
-            transform.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
